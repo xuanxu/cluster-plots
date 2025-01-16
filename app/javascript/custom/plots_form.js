1e99462b63d29eef2dcc36ba12ac4e63e97f9d31
Juanjo Bazán <jjbazan@gmail.com>
2025-01-16 11:57:37 +0100

    Sync stop time

--- app/javascript/custom/plots_form.js
+++ app/javascript/custom/plots_form.js
@@ -1,3 +1,13 @@
+// Update plot times
+function update_plot_times() {
+  var plot_times = document.getElementById("plot_times");
+  var start_date = document.getElementById("start_date");
+  var start_time = document.getElementById("start_time");
+  var stop_date = document.getElementById("stop_date");
+  var stop_time = document.getElementById("stop_time");
+  plot_times.innerHTML = start_date.value + "T" + start_time.value + "Z " + stop_date.value + "T" + stop_time.value + "Z";
+}
+
 // Show/hide list of panels checkboxes
 var toggle_panels = function() {
   var panels_listings = document.getElementById("panels");
@@ -10,9 +20,9 @@ var toggle_panels = function() {
     panels_heading.innerHTML = "Show panel selection <i class='fa fa-caret-right'></i>"
   }
 }
-
 document.getElementById("panels_heading").addEventListener('click', toggle_panels);
 
+// Update duration fields when start and stop times are changed
 var sync_duration = function() {
   var start_date = document.getElementById("start_date");
   var start_time = document.getElementById("start_time");
@@ -22,7 +32,6 @@ var sync_duration = function() {
   var duration_hours = document.getElementById("duration_hours");
   var duration_minutes = document.getElementById("duration_minutes");
   var duration_seconds = document.getElementById("duration_seconds");
-  var plot_times = document.getElementById("plot_times");
 
   var start = new Date(start_date.value + " " + start_time.value);
   var stop = new Date(stop_date.value + " " + stop_time.value);
@@ -42,11 +51,37 @@ var sync_duration = function() {
   duration_minutes.value = minutes;
   duration_seconds.value = seconds;
 
-  plot_times.innerHTML = start_date.value + "T" + start_time.value + "Z " + stop_date.value + "T" + stop_time.value + "Z";
+  update_plot_times();
   }
 }
-
 document.getElementById("start_date").addEventListener('change', sync_duration);
 document.getElementById("start_time").addEventListener('change', sync_duration);
 document.getElementById("stop_date").addEventListener('change', sync_duration);
 document.getElementById("stop_time").addEventListener('change', sync_duration);
+
+// Update stop time when duration is changed
+var sync_stop_time = function() {
+  var start_date = document.getElementById("start_date");
+  var start_time = document.getElementById("start_time");
+  var stop_date = document.getElementById("stop_date");
+  var stop_time = document.getElementById("stop_time");
+  var duration_days = document.getElementById("duration_days");
+  var duration_hours = document.getElementById("duration_hours");
+  var duration_minutes = document.getElementById("duration_minutes");
+  var duration_seconds = document.getElementById("duration_seconds");
+
+  var stop = new Date(start_date.value + " " + start_time.value + "Z");
+  stop.setDate(stop.getDate() + parseInt(duration_days.value));
+  stop.setHours(stop.getHours() + parseInt(duration_hours.value));
+  stop.setMinutes(stop.getMinutes() + parseInt(duration_minutes.value));
+  stop.setSeconds(stop.getSeconds() + parseInt(duration_seconds.value));
+
+  stop_date.value = stop.toISOString().slice(0, 10);
+  stop_time.value = stop.toISOString().slice(11, 19);
+
+  update_plot_times();
+}
+document.getElementById("duration_days").addEventListener('change', sync_stop_time);
+document.getElementById("duration_hours").addEventListener('change', sync_stop_time);
+document.getElementById("duration_minutes").addEventListener('change', sync_stop_time);
+document.getElementById("duration_seconds").addEventListener('change', sync_stop_time);

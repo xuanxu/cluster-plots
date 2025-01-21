7e3f776c1eb83c5a34dd821e36ca0e29cbc21205
Juanjo Bazán <jjbazan@gmail.com>
2025-01-21 12:12:32 +0100

    Add scope

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -1,4 +1,6 @@
 class Panel < ApplicationRecord
   enum :mission, cluster: 1, double_star: 2, data_mining: 3
   validates_uniqueness_of :name, scope: :mission
+
+  scope :ready, -> { where(active: true).where(stage: false) }
 end

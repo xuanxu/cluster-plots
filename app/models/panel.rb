bd21332191952a3fae3940a79dc33ce9ed4df015
Juanjo Bazán <jjbazan@gmail.com>
2025-01-22 07:49:15 +0100

    Order by instrument first

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -2,7 +2,7 @@ class Panel < ApplicationRecord
   enum :mission, cluster: 1, double_star: 2, data_mining: 3
   validates_uniqueness_of :name, scope: :mission
 
-  default_scope { order(order: :asc) }
+  default_scope { order(:experiment, order: :asc) }
   scope :ready, -> { where(active: true).where(stage: false) }
   scope :by_mission, ->(mission) { where(mission: mission) }
 end

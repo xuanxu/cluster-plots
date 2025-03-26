ba284a25ca2e8d24ab4cf85021ce5c64c8e20b9c
Juanjo Bazán <jjbazan@gmail.com>
2025-03-26 15:12:00 +0100

    Escape arguments on Python call

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -21,8 +21,21 @@ class Plot
       python #{Rails.root}/libext/run_panel2.py -b '#{start_datetime}' -e '#{end_datetime}' -p '#{panels}' -n 'test.ps' -t 'highcharts' -c 1 -j '#{json_file}' -o '#{start_datetime}/#{end_datetime}'
     PYPLOT
 
-    Rails.logger.info " * Running command: #{pycom}"
-    stdout_str, stderr_str, status = Open3.capture3 pycom
+    pycom = [
+      "python",
+      "#{Rails.root}/libext/run_panel2.py",
+      "-b", start_datetime,
+      "-e", end_datetime,
+      "-p", panels,
+      "-n", "test.ps",
+      "-t", "highcharts",
+      "-c", "1",
+      "-j", json_file,
+      "-o", "#{start_datetime}/#{end_datetime}"
+    ]
+
+    Rails.logger.info " * Running command: #{pycom.join(' ')}"
+    stdout_str, stderr_str, status = Open3.capture3(*pycom)
 
     if status.success?
       Oj.load_file("#{Rails.root}/libext/results/json_charts/#{json_file}")

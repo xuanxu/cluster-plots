b0f758ea2de8d80e1dcd66cda57a73b590648050
Juanjo Bazán <jjbazan@gmail.com>
2025-05-07 13:24:09 +0200

    Add axis chart

--- app/javascript/custom/charts.js
+++ app/javascript/custom/charts.js
@@ -1,13 +1,17 @@
+var title_chart, axis_chart;
+
 document.getElementById("plots").addEventListener("turbo:frame-render", function (event) {
   if (typeof(chart_json_data) !== undefined) {
     show_plot(chart_json_data, "highcharts_plot");
   }
-  if (typeof(chart_datetime_title) !== undefined) {
-    titleChart(chart_datetime_title);
+  if (typeof(chart_start_datetime) !== undefined) {
+    titleChart( chart_start_datetime + "/" + chart_stop_datetime);
   }
 })
 
 function show_plot(jsonData, targetDiv){
+  setHighchartsGlobalSettings();
+
   const json_panels = JSON.parse(jsonData);
   const json = json_panels.panels_arr[0].subpanels[0];
   const default_line_thickness = 2;
@@ -28,7 +32,7 @@ function show_plot(jsonData, targetDiv){
     ywidth = 150;
   }
   var subTop = 100 - json.size;
-  var axisLineWidth = 1;
+  var axisLineWidth = 2;
   var axisTickWidth = 2;
 
   var majorGridDisplay = 1;
@@ -96,7 +100,7 @@ function show_plot(jsonData, targetDiv){
     }
   }
 
-  new Highcharts.Chart({
+  var line_plot = new Highcharts.Chart({
     title: { text: "" },
     chart: {
       type: 'line',
@@ -130,12 +134,50 @@ function show_plot(jsonData, targetDiv){
         }
       }
     },
-    xAxis: {
-      type: 'datetime',
-      title: {
-        text: json_panels.panels_arr[0].xtitle || 'Time'
+    xAxis: [
+      {
+        type: 'datetime',
+        title: {
+          enable: false,
+        },
+        labels: { enabled: false },
+        startOnTick: false,
+        endOnTick: false,
+        minPadding: 0,
+        maxPadding: 0,
+        lineWidth: 2,
+        tickLength: 5,
+        tickWidth: 2,
+        minorTickLength: 2,
+        minorTickWidth: 1,
+        minorTickInterval: 'auto',
+        gridLineWidth: 1,
+        gridLineColor: '#ADD8E6',
+        lineColor: '#CBD6EA',
+        tickColor: '#CBD6EA',
+        minorTickColor: '#CBD6EA'
+      },
+      {
+        type: 'datetime',
+        opposite: true,
+        linkedTo: 0,
+        title: { enabled: false },
+        labels: { enabled: false },
+        startOnTick: false,
+        endOnTick: false,
+        minPadding: 0,
+        maxPadding: 0,
+        lineWidth: axisLineWidth,
+        tickLength: 5,
+        tickWidth: 2,
+        minorTickLength: 2,
+        minorTickWidth: 1,
+        gridLineWidth: 0,
+        lineColor: '#CBD6EA',
+        tickColor: '#CBD6EA',
+        minorTickColor: '#CBD6EA'
       }
-    },
+    ],
     yAxis: [
       {
         title: {
@@ -163,6 +205,12 @@ function show_plot(jsonData, targetDiv){
             }
           }
         },
+        plotLines: [{
+          color: '#A9A9A9',
+          width: 2,
+          value: 0,
+          dashStyle: 'ShortDash'
+        }],
         height: sub_height,
         top: subTop + '%',
         offset: 0,
@@ -180,6 +228,9 @@ function show_plot(jsonData, targetDiv){
         minorTickWidth: 1,
         gridLineWidth: majorGridDisplay,
         minorGridLineWidth: minorGridDisplay,
+        lineColor: '#CBD6EA',
+        tickColor: '#CBD6EA',
+        minorTickColor: '#CBD6EA'
       },
       { // axis on the opposite side to "close" the box
         title: { enabled: false },
@@ -202,17 +253,23 @@ function show_plot(jsonData, targetDiv){
         minorTickWidth: 1,
         gridLineWidth: majorGridDisplay,
         minorGridLineWidth: minorGridDisplay,
-        gridZIndex: 1
+        gridZIndex: 1,
+        lineColor: '#CBD6EA',
+        tickColor: '#CBD6EA',
+        minorTickColor: '#CBD6EA'
       }
     ],
     // data
     series: series,
   });
+
+  var axis_start_datetime = line_plot.xAxis[0].getExtremes().min;
+  var axis_stop_datetime = line_plot.xAxis[0].getExtremes().max;
+  axisChart(axis_start_datetime, axis_stop_datetime)
 }
 
 function titleChart(titleText) {
-
-  var title_chart = new Highcharts.Chart({
+  title_chart = new Highcharts.Chart({
     chart: {
       renderTo: "title-chart"
     },
@@ -229,6 +286,122 @@ function titleChart(titleText) {
       }
     }
   });
+}
 
+function axisChart(start,stop) {
+  axis_chart = new Highcharts.Chart({
+    title: { enabled: false, text: "" },
+    chart: {
+      renderTo: 'axis-chart',
+      height: 50,
+      marginBottom: 40,
+      marginTop: 0,
+    },
+    exporting: {
+      sourceWidth: 1000,
+      sourceHeight: 40
+    },
+    yAxis: [{
+      showEmpty: true,
+      title: { enabled: false }
+    }],
+    xAxis: [{
+      type: 'datetime',
+      min: start,
+      max: stop,
+      showEmpty: true,
+      gridLineWidth: 0,
+      minorGridLineWidth: 0,
+      labels: {
+        enabled: true,
+        style: {
+          fontSize: '12px',
+        },
+        formatter: function () {
+          return Highcharts.dateFormat('%H:%M:%S', this.value);
+        }
+      },
+      lineWidth: 2,
+      tickWidth: 2,
+      tickLength: 5,
+      minorTickLength: 2,
+      minorTickWidth: 2,
+      minorTickInterval: 'auto',
+      lineColor: '#CBD6EA',
+      tickColor: '#CBD6EA',
+      minorTickColor: '#CBD6EA'
+    }]
+  });
 
+  window["axis_chart"] = axis_chart;
+  var line = {
+    color: 'black',
+    data: [],
+    showInLegend: false
+  }
+  axis_chart.addSeries(line, false);
+  axis_chart.redraw();
+}
+
+function setHighchartsGlobalSettings(){
+  Highcharts.setOptions({
+    global: {
+      time: { useUTC: true }
+    },
+    chart: {
+      backgroundColor:'white',
+      borderColor: '#003399',
+      borderWidth: 0,
+      margin: [10, 120, 10, 120],
+      height: 250,
+      // hide highcharts default 'reset zoom' button
+      resetZoomButton: {
+        theme: { display: 'none' }
+      },
+    },
+    title: {
+      enabled: false,
+      text: ''
+    },
+    exporting: {
+      allowHTML: true,
+      sourceWidth: 1000,
+      sourceHeight: 300,
+      fallbackToExportServer: false, // Ensure the export happens on the client side or not at all
+      buttons: {
+        contextButton: {
+          enabled: false // hide default highcharts download button
+        }
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    legend: {
+      layout: 'vertical',
+      reversed: true,
+      borderWidth: 0,
+      floating: true,
+      align: 'right',
+      verticalAlign: 'middle',
+      x: -5,
+      y: 10
+    },
+    labels: {
+      align: 'right',
+      x: -10,
+      y: 0
+    },
+    tooltip: { enabled: false },
+    plotOptions: {
+      enableMouseTracking: false,
+      series: {
+        states: {
+          inactive: { opacity: 1 }, // to cancel serie highlight on mouse hover need to set opacity to 1
+          hover: { enabled: false  },//disable any styling of a currently hovered line series => don't want the markers to be highlighted when hovering on the line
+         }
+      },
+    },
+    series: []
+  });
 }

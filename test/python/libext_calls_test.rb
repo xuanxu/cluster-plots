5012a6f5ab54e3b5c61fb9edda8942718a76fa10
Juanjo Bazán <jjbazan@gmail.com>
2025-09-05 11:48:56 +0200

    Fix test call

--- test/python/libext_calls_test.rb
+++ test/python/libext_calls_test.rb
@@ -4,7 +4,7 @@ require "open3"
 class LibextCallsTest < ActiveSupport::TestCase
   test "calling run_panel2" do
     pycom =<<-PYPLOT
-    python #{Rails.root}/libext/run_panel2.py -b '2016-09-11T00:00:00Z' -e '2016-09-11T01:15:00Z' -p 'C4_CG_CIS_NI_CAA' -n 'test.ps' -t 'highcharts' -c 1 -j 'testC42016X.json' -o '2016-09-11T00:00:00Z/2016-09-11T01:15:00Z'
+    python #{Rails.root}/libext/run_panel2.py -b '2016-09-11T00:00:00Z' -e '2016-09-11T01:15:00Z' -p 'C4_CG_CIS_NI_CAA' -n 'test.ps' -t 'highcharts' -j 'testC42016X.json' -o '2016-09-11T00:00:00Z/2016-09-11T01:15:00Z'
     PYPLOT
 
     stdout_str, stderr_str, status = Open3.capture3 pycom
